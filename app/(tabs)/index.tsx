4fb5d53cb3306544b690d7b5e9980fff1df8ff44
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2024-06-10 23:06:34 -0300

    :tada: Init project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
+import { IconButton } from '@/components/IconButton';
+import Draggable from 'react-native-draggable';
+
+const lixeiraAzul = require('../../assets/images/app/lata-azul.png');
+const lixeiraVermelha = require('../../assets/images/app/lata-vermelha.png');
+const lixeiraAmarela = require('../../assets/images/app/lata-amarela.png');
+const lixeiraMarrom = require('../../assets/images/app/lata-marrom.png');
+const lixeiraVerde = require('../../assets/images/app/lata-verde.png');
+
+const achocolatado = require('../../assets/images/app/achocolatado.png');
+const pet = require('../../assets/images/app/pet.png');
+
 
 export default function TabOneScreen() {
+
+  const handleTrashcanSelection = (trashcanType: string) => {
+    console.log(trashcanType)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      
+      <View style={styles.itemsContainer}>
+        <Draggable x={0} y={0} imageSource={pet} renderSize={80} shouldReverse />
+        <Draggable x={90} y={0} imageSource={achocolatado} renderSize={80} shouldReverse/>
+      </View>
+
+      <View style={styles.trashCanContainer}>
+        <View style={styles.trashCanRow}>
+          <IconButton icon={lixeiraAzul} selected={false} onPress={() => handleTrashcanSelection('papel')}/>
+          <IconButton icon={lixeiraVermelha} selected={false} onPress={() => handleTrashcanSelection('plastico')}/>
+          <IconButton icon={lixeiraAmarela} selected={false} onPress={() => handleTrashcanSelection('metal')}/>
+        </View>
+
+        <View style={styles.trashCanRow}>
+          <IconButton icon={lixeiraVerde} selected={false} onPress={() => handleTrashcanSelection('vidro')}/>
+          <IconButton icon={lixeiraMarrom} selected={false} onPress={() => handleTrashcanSelection('organico')}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,16 +47,25 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  itemsContainer: {
+    marginTop: 50,
+    width: '100%'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+
+  trashCanContainer: {
+    marginBottom: 50,
+    zIndex: -1
   },
+
+  trashCanRow: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
 });
---
+++ components/IconButton.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Image, TouchableOpacity, StyleSheet, ImageSourcePropType } from "react-native";
+
+interface IIconProps {
+    icon: ImageSourcePropType,
+    selected: boolean,
+    onPress: () => void,
+    clear: 
+}
+
+const buttonWidth = 80;
+const buttonHeight = 80;
+
+export function IconButton({ onPress, selected, icon}: IIconProps) {
+    const [isSelected, setIsSelected] = useState(selected);
+
+    return (
+        <TouchableOpacity onPressOut={() => {
+            if(!isSelected) {
+                onPress()
+            }
+            setIsSelected(!isSelected)
+        }}>
+            <Image source={icon} style={isSelected ? styles.trashcanSelected : styles.trashcan}/>
+        </TouchableOpacity>
+    );
+} 
+
+const styles = StyleSheet.create({
+    trashcan: {
+        width: buttonWidth,
+        height: buttonHeight,
+        margin: 20,
+    },
+
+    trashcanSelected: {
+        width: buttonWidth,
+        height: buttonHeight,
+        margin: 20,
+        borderColor: 'yellow',
+        borderWidth: 5,
+    } 
+})
